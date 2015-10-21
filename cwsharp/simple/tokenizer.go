45f7cfa891bcd2fae736ed4f7dabad741a80049c
zhengchun <zhengchunster@gmail.com>
2015-10-21 17:57:42 +0800

    支持英文+数字，连接符，浮点数字

--- cwsharp/simple/tokenizer.go
+++ cwsharp/simple/tokenizer.go
@@ -1,21 +1,16 @@
 // 英文分词包，提供对包含英文、数字或字符分隔符组成的字符串分词
+// 支持下列混合格式:12-ab,ak49,12.1,P.M?
+
 package simple
 
 import (
-	"errors"
 	"fmt"
 	"github.com/zhengchun/cwsharp-go/cwsharp"
 	"unicode"
 )
 
-var done error = errors.New("No more token to read.")
-
-// 默认实例对象
-var Default *Tokenizer = &Tokenizer{DefaultCheckContinue}
-
 type Tokenizer struct {
-	// 自定义字符组合的策略
-	CheckContinue func(r cwsharp.Reader, ch rune, via []rune) (cwsharp.Kind, bool)
+	//CheckContinue func(r cwsharp.Reader, ch rune, via []rune) (cwsharp.Kind, bool)
 }
 
 type Token struct {
@@ -42,9 +37,9 @@ type TokenIterator struct {
 }
 
 func (t *Tokenizer) Traverse(r cwsharp.Reader) cwsharp.TokenIterator {
-	if t.CheckContinue == nil {
-		t.CheckContinue = DefaultCheckContinue
-	}
+	//if t.CheckContinue == nil {
+	//	t.CheckContinue = DefaultCheckContinue
+	//}
 	return &TokenIterator{
 		reader: r,
 		p:      t,
@@ -56,7 +51,7 @@ func New() *Tokenizer {
 }
 
 func isPunct(c rune) bool {
-	return unicode.IsSpace(c) || unicode.IsPunct(c)
+	return unicode.IsSpace(c) || unicode.IsPunct(c) || unicode.IsSymbol(c)
 }
 
 func isLetter(c rune) bool {
@@ -67,85 +62,80 @@ func isCjk(c rune) bool {
 	return unicode.Is(unicode.Scripts["Han"], c)
 }
 
-func checkContinueWithPunct(r cwsharp.Reader, ch rune, via []rune) (cwsharp.Kind, bool) {
-	if ch == ' ' || len(via) == 0 || r.Peek() == cwsharp.EOF {
-		return cwsharp.PUNCT, false
-	}
-	return cwsharp.PUNCT, false
-}
-
-func DefaultCheckContinue(r cwsharp.Reader, ch rune, via []rune) (cwsharp.Kind, bool) {
-	if isPunct(ch) {
-		return checkContinueWithPunct(r, ch, via)
-	} else if isLetter(ch) {
-		for _, c := range via {
-			if unicode.IsNumber(c) {
-				return cwsharp.ALPHANUM, true
-			}
-		}
-		return cwsharp.LETTER, true
-	} else if unicode.IsNumber(ch) {
-		for _, c := range via {
-			if unicode.IsLetter(c) {
-				return cwsharp.ALPHANUM, true
-			}
-		}
-		return cwsharp.NUMERAL, true
-	} else if isCjk(ch) {
-		return cwsharp.CJK, false
-	}
-	return cwsharp.PUNCT, false
-
-}
-
-func (iter *TokenIterator) nextToken() (cwsharp.Token, error) {
-	var err error
-	t := &Token{}
-	var via []rune
-	r := iter.reader
-	for ch, d := r.ReadRule(), 0; ch != cwsharp.EOF; ch = r.ReadRule() {
-		ch = normalizeRule(ch)
-		k, ok := iter.p.CheckContinue(r, ch, via)
-		if ok {
-			via = append(via, ch)
-			t.kind = k
-		} else {
-			if d == 0 {
-				via = append(via, ch)
-				t.kind = k
-			} else {
-				r.Seek(r.Pos() - 1)
-			}
-			break
-		}
-		d++
-	}
-	if len(via) == 0 {
-		err = done
-	}
-	t.text = string(via)
-	return t, err
+func isNumer(c rune) bool {
+	return unicode.IsNumber(c)
 }
 
 func (iter *TokenIterator) Next() bool {
-	t, err := iter.nextToken()
-	if err != nil || err == done {
+	if iter.reader.Peek() == cwsharp.EOF {
 		return false
+	}		
+	ch := iter.reader.ReadRule()
+	if isCjk(ch) {
+		iter.ch = &Token{string(ch), cwsharp.CJK}
+		return true
 	}
-	iter.ch = t
+	var kind cwsharp.Kind
+	var via []rune
+	if isLetter(ch){
+		for ch != cwsharp.EOF {
+			if isLetter(ch) {
+				via = append(via, ch)
+				kind |= cwsharp.LETTER
+			} else if isNumer(ch) {
+				via = append(via, ch)
+				kind |= cwsharp.NUMERAL
+			} else {
+				nextch := iter.reader.Peek()
+				if ch == '-' && (isNumer(nextch) || isLetter(nextch)) {
+					via = append(via, ch)
+					kind |= cwsharp.LETTER
+				} else {
+					break
+				}
+			}
+			ch = iter.reader.ReadRule()
+		}
+		iter.ch = &Token{string(via), kind}
+		return true
+	} else if isNumer(ch) {
+		times := 0
+		endIndex := 0
+		offset:=iter.reader.Pos()
+		for i := 0; ch != cwsharp.EOF; i++ {
+			if isNumer(ch) {
+				via = append(via, ch)
+				kind |= cwsharp.NUMERAL
+			} else if isLetter(ch) {
+				via = append(via, ch)
+				kind |= cwsharp.LETTER
+			} else if ch == '.'{
+				if kind&cwsharp.LETTER == cwsharp.LETTER{
+					via = via[:endIndex]
+					iter.reader.Seek(iter.reader.Pos() - 1)
+					break
+				}else if times>0{
+					//BUG 2015.1.1
+					via = via[:endIndex]
+					iter.reader.Seek(offset+endIndex-1)
+					break
+				}
+				via = append(via, ch)
+				endIndex = i
+				times++
+			} else {				
+				iter.reader.Seek(iter.reader.Pos() - 1)
+				break
+			}
+			ch = iter.reader.ReadRule()
+		}
+		iter.ch = &Token{string(via), kind}
+		return true
+	}
+	iter.ch = &Token{string(ch), cwsharp.PUNCT}
 	return true
 }
 
 func (iter *TokenIterator) Cur() cwsharp.Token {
 	return iter.ch
-}
-
-// 大写字母转化小化、全角转半角
-func normalizeRule(r rune) rune {
-	if unicode.IsUpper(r) {
-		return r + 32
-	} else if r >= 0xff01 && r <= 0xff5d {
-		return r - 0xFEE0
-	}
-	return r
 }
--- cwsharp/simple/tokenizer_test.go
+++ cwsharp/simple/tokenizer_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test(t *testing.T) {
-	var input string = "Hello,World!你好，世界!"
+	var input string = "Hello,World!你好，世界!14.4%,abc1233,11a-b,2015.2.1"
 	tokenizer := New()
 	for iter := tokenizer.Traverse(cwsharp.NewStringReader(input)); iter.Next(); {
 		fmt.Println(iter.Cur())
