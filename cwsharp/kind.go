2923b17d8e63b28ff1ce3a6948bb01feff0e9655
zhengchun <zhengchunster@gmail.com>
2015-10-21 17:59:00 +0800

    删除ALPHANUM枚举

--- cwsharp/kind.go
+++ cwsharp/kind.go
@@ -3,36 +3,36 @@ package cwsharp
 type Kind int
 
 const (
-	PUNCT    Kind = 1 //标号
-	NUMERAL  Kind = 2 //数字
-	LETTER   Kind = 4 //字母
-	CJK      Kind = 8 //中文
-	ALPHANUM Kind = NUMERAL | LETTER
+	PUNCT   Kind = 1 //标号
+	NUMERAL Kind = 2 //数字
+	LETTER  Kind = 4 //字母
+	CJK     Kind = 8 //中文
 )
 
 func (k Kind) String() string {
 	t := "UNKNOW"
+	if k&NUMERAL==NUMERAL && k&LETTER==LETTER{
+		t="ALPHANUM"
+	}else{
 	switch k {
-	case PUNCT:
-		{
-			t = "PUNCT"
-		}
-	case ALPHANUM:
-		{
-			t = "ALPHANUM"
-		}
-	case NUMERAL:
-		{
-			t = "NUMERAL"
-		}
-	case LETTER:
-		{
-			t = "LETTER"
-		}
-	case CJK:
-		{
-			t = "CJK"
+		case PUNCT:
+			{
+				t = "PUNCT"
+			}
+		case NUMERAL:
+			{
+				t = "NUMERAL"
+			}
+		case LETTER:
+			{
+				t = "LETTER"
+			}
+		case CJK:
+			{
+				t = "CJK"
+			}
 		}
 	}
+
 	return t
 }
